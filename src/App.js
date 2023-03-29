bd42020962034245fddea0f0838e89c5a674455b
genia <guk.genia@gmail.com>
2023-03-29 10:17:55 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import { Button } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+      <div>
+        <h1> Hello</h1>
+        <Button variant="primary">Button</Button>
+      </div>
+  );
+}
+
+export default App;
